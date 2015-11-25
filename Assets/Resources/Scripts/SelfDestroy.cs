a86df38174ec1b11efe4c9c6304441de82fd64fb
EdouardAngebault <edouard.angebault@gmail.com>
2015-11-25 17:27:30 +0100

    Menu updated with buttons skin

---
+++ Assets/Resources/Scripts/SelfDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour {
+
+	public float delay;
+
+	void Start () {
+		StartCoroutine (timerDestroy());
+	}
+	
+	IEnumerator timerDestroy()
+	{			
+		yield return new WaitForSeconds(delay);
+		Destroy (this.gameObject);
+	}
+}
