bf9819c04407a5e9aeec67ad747ff8ff498f4300
EdouardAngebault <edouard.angebault@gmail.com>
2015-12-01 11:35:43 +0100

    Added Garden Props + Lantern animation + Hero of the Wind

--- Assets/Resources/Scripts/SelfRotate.cs
+++ Assets/Resources/Scripts/SelfRotate.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class SelfRotate : MonoBehaviour {
-
-	public float speed = 2;
+	
+	public float RightSpeed = 2;
+	public float UpSpeed = 2;
 
 	void Update() {
-		transform.Rotate(Vector3.right * 3 * Time.deltaTime * speed);
-		transform.Rotate(Vector3.up * 6 * Time.deltaTime * speed, Space.World);
+		transform.Rotate(Vector3.right * 3 * Time.deltaTime * RightSpeed);
+		transform.Rotate(Vector3.up * 6 * Time.deltaTime * UpSpeed);
 	}
 }
